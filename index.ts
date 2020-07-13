f029e0be3a9b0251eb90d5dd4496cc061211585b
adcpm <fabien@bonustrack.co>
2020-07-14 02:27:31 +0700

    Fix favicon

--- index.ts
+++ index.ts
@@ -8,10 +8,10 @@ import api from './server/api';
 export default (app, server) => {
   app.use(bodyParser.json({ limit: '20mb' }));
   app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
-  app.use(frameguard({ action: 'deny' }));
-  app.use(cors());
   // @ts-ignore
   app.use(serveStatic(`${__dirname}/dist`));
+  app.use(frameguard({ action: 'deny' }));
+  app.use(cors());
   app.use('/api', api);
   // @ts-ignore
   app.get('/', (req, res) => res.sendFile(`${__dirname}/dist/index.html`));
