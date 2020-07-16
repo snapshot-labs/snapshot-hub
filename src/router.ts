f289baa121a11cc57062ccaa3a9084be071538a8
adcpm <fabien@bonustrack.co>
2020-07-16 13:36:09 +0700

    Add homepage

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
+import Home from '@/views/Home.vue';
 import Proposals from '@/views/Proposals.vue';
 import Proposal from '@/views/Proposal.vue';
 import Create from '@/views/Create.vue';
@@ -7,11 +8,7 @@ import Create from '@/views/Create.vue';
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'home',
-    beforeEnter: (to, from, next) => next('/balancer')
-  },
+  { path: '/', name: 'home', component: Home },
   { path: '/:key/proposal/:id', name: 'proposal', component: Proposal },
   { path: '/:key/create', name: 'create', component: Create },
   { path: '/:key', name: 'proposals', component: Proposals },
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Container>
+      <h1 class="mb-3">Explore</h1>
+    </Container>
+    <Container :slim="true">
+      <router-link
+        v-for="token in tokens"
+        :key="token.token"
+        :to="{ name: 'proposals', params: { key: token.key } }"
+      >
+        <Block class="text-center">
+          <Token :address="token.token" size="128" class="mb-4" />
+          <div>
+            <h2>{{ token.name }}</h2>
+            <h3>{{ token.symbol }}</h3>
+          </div>
+        </Block>
+      </router-link>
+    </Container>
+  </div>
+</template>
+
+<script>
+import tokens from '@/helpers/tokens.json';
+
+export default {
+  data() {
+    return {
+      tokens
+    };
+  }
+};
+</script>
