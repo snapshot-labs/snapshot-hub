6a2844ca93b8ec38c349286dd994117270132f29
adcpm <fabien@bonustrack.co>
2020-07-19 18:47:02 +0700

    Clean lock integration

--- src/helpers/lock.ts
+++ src/helpers/lock.ts
@@ -1,11 +1,11 @@
-import l from '@bonustrack/lock/dist/lock.esm';
+import { Lock } from '@bonustrack/lock/dist/lock.cjs';
 import config from '@/helpers/config';
 import injected from '@bonustrack/lock/connectors/injected';
 import portis from '@bonustrack/lock/connectors/portis';
 import walletconnect from '@bonustrack/lock/connectors/walletconnect';
 
 const connectors = { injected, portis, walletconnect };
-const lock = new l.Lock(connectors);
+const lock = new Lock();
 Object.entries(config.connectors).forEach((connector: any) => {
   lock.addConnector({
     key: connector[0],
