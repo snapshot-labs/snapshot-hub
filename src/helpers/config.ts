521c375fb0aea6a45601de578105a7934c980544
adcpm <fabien@bonustrack.co>
2020-07-23 22:43:43 +0700

    Fix config id

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -3,6 +3,6 @@ import config from '@/config.json';
 let id = 'master';
 const domainName = window.location.hostname;
 if (domainName.includes('localhost:')) id = 'local';
-if (domainName === 'beta.vote.balancer.finance') id = 'beta';
+if (domainName === 'beta.vote.balancer.finance') id = 'develop';
 
 export default config[id];
