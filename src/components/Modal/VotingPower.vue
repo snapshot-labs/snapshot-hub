54998b27d1089867cf22752f35456907741b940f
adcpm <fabien@bonustrack.co>
2020-07-24 01:27:05 +0700

    Improve voting power modal

--- src/components/Modal/VotingPower.vue
+++ src/components/Modal/VotingPower.vue
@@ -1,20 +1,10 @@
 <template>
   <UiModal :open="open" @close="$emit('close')">
     <h3 class="m-4 mb-0 text-center">Your voting power</h3>
-    <h2 class="m-4 mb-0 p-4 border rounded-2 text-center">
-      {{ _numeral(gov.votingPower) }} {{ gov.namespace.symbol }}
-    </h2>
     <div class="m-4 p-4 border rounded-2 text-white">
-      <div class="d-flex">
-        <span v-text="'Snapshot'" class="flex-auto text-gray mr-1" />
-        <a :href="_etherscanLink(gov.snapshot, 'block')" target="_blank">
-          {{ $n(gov.snapshot) }}
-          <Icon name="external-link" class="ml-1" />
-        </a>
-      </div>
-      <div class="d-flex">
-        <span v-text="'Total voting power'" class="flex-auto text-gray mr-1" />
-        {{ _numeral(gov.votingPower) }} {{ gov.namespace.symbol }}
+      <div class="d-flex mb-3 pb-2 border-bottom" style="font-size: 22px;">
+        <b v-text="'Total voting power'" class="flex-auto text-gray mr-1" />
+        <b>{{ _numeral(gov.votingPower) }} {{ gov.namespace.symbol }} </b>
       </div>
       <div class="d-flex">
         <span v-text="'Wallet balance'" class="flex-auto text-gray mr-1" />
@@ -33,6 +23,13 @@
           target="_blank"
         >
           {{ $n(pool) }} {{ gov.namespace.symbol }}
+          <Icon name="external-link" class="ml-1" />
+        </a>
+      </div>
+      <div class="d-flex mt-3">
+        <span v-text="'Snapshot block number'" class="flex-auto text-gray mr-1" />
+        <a :href="_etherscanLink(gov.snapshot, 'block')" target="_blank">
+          {{ $n(gov.snapshot) }}
           <Icon name="external-link" class="ml-1" />
         </a>
       </div>
