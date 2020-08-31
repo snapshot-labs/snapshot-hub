000f3280cbcf644c5d42b0958310deb5d9e796c7
bonustrack <fabien@bonustrack.co>
2020-08-31 14:38:50 +0700

    Rollback to Fleek

--- server/ipfs.ts
+++ server/ipfs.ts
@@ -1,29 +1,24 @@
-/*
 import fleek from '@fleekhq/fleek-storage-js';
+import pinataSDK from '@pinata/sdk';
 
+const service = 'fleek';
+
+const pinata = pinataSDK(process.env.PINATA_API_KEY, process.env.PINATA_SECRET_API_KEY);
 const config: any = {
   apiKey: process.env.FLEEK_API_KEY,
   apiSecret: process.env.FLEEK_API_SECRET
 };
 
 export async function pinJson(key: string, body) {
-  const input = config;
-  input.key = key;
-  input.data = JSON.stringify(body);
-  const result = await fleek.upload(input);
-  return result.hashV0;
-}
-*/
-
-import pinataSDK from '@pinata/sdk';
-
-const pinata = pinataSDK(process.env.PINATA_API_KEY, process.env.PINATA_SECRET_API_KEY);
-
-export async function pinJson(key: string, body) {
-  try {
+  // @ts-ignore
+  if (service === 'fleek') {
+    const input = config;
+    input.key = key;
+    input.data = JSON.stringify(body);
+    const result = await fleek.upload(input);
+    return result.hashV0;
+  } else {
     const result = await pinata.pinJSONToIPFS(body);
     return result.IpfsHash;
-  } catch (e) {
-    console.log('Failed to pin to IPFS', e);
   }
 }
