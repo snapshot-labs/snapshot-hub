4c4e3a727bb1bee3d256437e69539634babc7f95
bonustrack <fabien@bonustrack.co>
2020-08-31 13:56:31 +0700

    Catch error

--- server/ipfs.ts
+++ server/ipfs.ts
@@ -20,6 +20,10 @@ import pinataSDK from '@pinata/sdk';
 const pinata = pinataSDK(process.env.PINATA_API_KEY, process.env.PINATA_SECRET_API_KEY);
 
 export async function pinJson(key: string, body) {
-  const result = await pinata.pinJSONToIPFS(body);
-  return result.IpfsHash;
+  try {
+    const result = await pinata.pinJSONToIPFS(body);
+    return result.IpfsHash;
+  } catch (e) {
+    console.log('Failed to pin to IPFS', e);
+  }
 }
