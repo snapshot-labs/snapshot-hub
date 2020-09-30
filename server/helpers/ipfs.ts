e1ba2eda2eb93df2f6a761bba9af3cfc24aa4c24
superbeefyboy <superbeefyboy@pm.me>
2020-09-30 12:40:22 -0300

    pinata based IPFS management

--- server/helpers/ipfs.ts
+++ server/helpers/ipfs.ts
@@ -1,24 +1,9 @@
 import fleek from '@fleekhq/fleek-storage-js';
 import pinataSDK from '@pinata/sdk';
 
-const service = process.env.PINNING_SERVICE || 'pinata';
-
-const pinata = pinataSDK(process.env.PINATA_API_KEY, process.env.PINATA_SECRET_API_KEY);
-const config: any = {
-  apiKey: process.env.FLEEK_API_KEY,
-  apiSecret: process.env.FLEEK_API_SECRET
-};
+const pinata = pinataSDK(process.env.PINATA_KEY, process.env.PINATA_SECRET);
 
 export async function pinJson(key: string, body) {
-  // @ts-ignore
-  if (service === 'fleek') {
-    const input = config;
-    input.key = key;
-    input.data = JSON.stringify(body);
-    const result = await fleek.upload(input);
-    return result.hashV0;
-  } else {
-    const result = await pinata.pinJSONToIPFS(body);
-    return result.IpfsHash;
-  }
+  const result = await pinata.pinJSONToIPFS(body);
+  return result.IpfsHash;
 }
