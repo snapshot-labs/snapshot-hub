5dc91af8dafe4cae3ee7aa9073d2cbb53bc69a04
bonustrack <fabien@bonustrack.co>
2020-10-17 11:12:41 +0700

    Add support for EIP-1271

---
+++ server/helpers/eip1271.ts
@@ -0,0 +1,51 @@
+// https://github.com/WalletConnect/walletconnect-example-dapp/blob/961727c2fe4e33587bd1543393afa51033f95a4e/src/helpers/eip1271.ts
+import { Contract, providers, utils } from 'ethers';
+
+export const spec = {
+  magicValue: '0x1626ba7e',
+  abi: [
+    {
+      constant: true,
+      inputs: [
+        {
+          name: '_hash',
+          type: 'bytes32'
+        },
+        {
+          name: '_sig',
+          type: 'bytes'
+        }
+      ],
+      name: 'isValidSignature',
+      outputs: [
+        {
+          name: 'magicValue',
+          type: 'bytes4'
+        }
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function'
+    }
+  ]
+};
+
+export async function isValidSignature(
+  address: string,
+  sig: string,
+  data: string,
+  provider: providers.Provider,
+  abi = spec.abi,
+  magicValue = spec.magicValue
+): Promise<boolean> {
+  let returnValue;
+  try {
+    returnValue = await new Contract(address, abi, provider).isValidSignature(
+      utils.arrayify(data),
+      sig
+    );
+  } catch (e) {
+    return false;
+  }
+  return returnValue.toLowerCase() === magicValue.toLowerCase();
+}
--- server/helpers/utils.ts
+++ server/helpers/utils.ts
@@ -1,4 +1,8 @@
 import { verifyMessage } from '@ethersproject/wallet';
+import { providers } from 'ethers';
+import { convertUtf8ToHex } from '@walletconnect/utils';
+import * as ethUtil from 'ethereumjs-util';
+import { isValidSignature } from './eip1271';
 
 export function jsonParse(input, fallback?) {
   try {
@@ -22,4 +26,58 @@ export function sendError(res, description) {
     error: 'unauthorized',
     error_description: description
   });
+}
+
+export function recoverPublicKey(sig: string, hash: string): string {
+  const params = ethUtil.fromRpcSig(sig);
+  const result = ethUtil.ecrecover(
+    ethUtil.toBuffer(hash),
+    params.v,
+    params.r,
+    params.s
+  );
+  return ethUtil.bufferToHex(ethUtil.publicToAddress(result));
+}
+
+export async function verifySignature(
+  address: string,
+  sig: string,
+  hash: string
+  // chainId: number
+): Promise<boolean> {
+  const rpcUrl =
+    'https://eth-mainnet.alchemyapi.io/v2/4bdDVB5QAaorY2UE-GBUbM2yQB3QJqzv';
+  const provider = new providers.JsonRpcProvider(rpcUrl);
+  const bytecode = await provider.getCode(address);
+  if (
+    !bytecode ||
+    bytecode === '0x' ||
+    bytecode === '0x0' ||
+    bytecode === '0x00'
+  ) {
+    const signer = recoverPublicKey(sig, hash);
+    return signer.toLowerCase() === address.toLowerCase();
+  } else {
+    console.log('Smart contract signature');
+    return isValidSignature(address, sig, hash, provider);
+  }
+}
+
+export function encodePersonalMessage(msg: string): string {
+  const data = ethUtil.toBuffer(convertUtf8ToHex(msg));
+  const buf = Buffer.concat([
+    Buffer.from(
+      '\u0019Ethereum Signed Message:\n' + data.length.toString(),
+      'utf8'
+    ),
+    data
+  ]);
+  return ethUtil.bufferToHex(buf);
+}
+
+export function hashPersonalMessage(msg: string): string {
+  const data = encodePersonalMessage(msg);
+  const buf = ethUtil.toBuffer(data);
+  const hash = ethUtil.keccak256(buf);
+  return ethUtil.bufferToHex(hash);
 }
