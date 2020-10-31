cf89d7e56a303d65ef17095ed29f8660d1bf63ec
superbeefyboy <superbeefyboy@pm.me>
2020-10-31 08:44:40 -0300

    testing webhook live

--- server/helpers/discord.ts
+++ server/helpers/discord.ts
@@ -1,6 +1,8 @@
 import Discord from 'discord.js';
 
 const client = new Discord.WebhookClient(process.env.DISCORD_HOOK_ID, process.env.DISCORD_HOOK_TOKEN);
+client.send('testing webhook');
+
 
 export const sendMessage = message => {
   try {
