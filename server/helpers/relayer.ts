29a3f7ec055bd6de4f74aec3cc208035b87160b5
Chaitanya <yourchaitu@gmail.com>
2021-04-14 21:42:43 +0530

    Fix process env

--- server/helpers/relayer.ts
+++ server/helpers/relayer.ts
@@ -1,12 +1,6 @@
 import { Wallet } from '@ethersproject/wallet';
 
-declare var process : {
-  env: {
-    RELAYER_PK: string
-  }
-}
-
-const privateKey = process.env.RELAYER_PK;
+const privateKey = process.env.RELAYER_PK || '';
 const wallet = new Wallet(privateKey);
 
 export default wallet;
