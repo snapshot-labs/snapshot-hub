e3d196e62e5fb40f81520eccf9d1758344003246
Fabien <bonustrack@users.noreply.github.com>
2021-04-21 19:24:49 +0700

    Update mysql.ts

--- server/helpers/mysql.ts
+++ server/helpers/mysql.ts
@@ -6,7 +6,7 @@ import parse from 'connection-string';
 
 // @ts-ignore
 const config = parse(process.env.JAWSDB_URL);
-config.connectionLimit = 25;
+config.connectionLimit = 50;
 config.multipleStatements = true;
 config.database = config.path[0];
 config.host = config.hosts[0].name;
