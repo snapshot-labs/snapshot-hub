315f8a3de052803b8e4c715441ee039971e7a23a
Fabien <bonustrack@users.noreply.github.com>
2021-04-22 02:43:50 +0700

    Allow query proposal by id (#53)

--- server/graphql/index.ts
+++ server/graphql/index.ts
@@ -9,30 +9,35 @@ const schemaFile = importSchema('./**/*.graphql');
 export const schema = buildSchema(schemaFile);
 
 export const rootValue = {
-  timeline: async ({ spaces = [], first = 10, skip = 0, state }) => {
+  timeline: async ({ first = 10, skip = 0, id, spaces = [], state }) => {
     const ts = parseInt((Date.now() / 1e3).toFixed());
     if (spaces.length === 0) spaces = Object.keys(registrySpaces) as any;
 
+    let queryStr = '';
     const params: any[] = [1614473607, spaces];
 
-    let stateStr = '';
+    if (id) {
+      queryStr += `AND id = ? `;
+      params.push(id);
+    }
+
     if (state === 'pending') {
-      stateStr = 'AND JSON_EXTRACT(payload, "$.start") > ?';
+      queryStr += 'AND JSON_EXTRACT(payload, "$.start") > ? ';
       params.push(ts);
     }
     if (state === 'active') {
-      stateStr =
-        'AND JSON_EXTRACT(payload, "$.start") < ? AND JSON_EXTRACT(payload, "$.end") > ?';
+      queryStr +=
+        'AND JSON_EXTRACT(payload, "$.start") < ? AND JSON_EXTRACT(payload, "$.end") > ? ';
       params.push(ts, ts);
     }
     if (state === 'closed') {
-      stateStr = 'AND ? > JSON_EXTRACT(payload, "$.end")';
+      queryStr += 'AND ? > JSON_EXTRACT(payload, "$.end") ';
       params.push(ts);
     }
 
     params.push(skip, first);
 
-    const query = `SELECT * FROM messages WHERE type = 'proposal' AND timestamp > ? AND space IN (?) ${stateStr} ORDER BY timestamp DESC LIMIT ?, ?`;
+    const query = `SELECT * FROM messages WHERE type = 'proposal' AND timestamp > ? AND space IN (?) ${queryStr} ORDER BY timestamp DESC LIMIT ?, ?`;
     const msgs = await db.queryAsync(query, params);
 
     const authors = Array.from(new Set(msgs.map(msg => msg.address)));
