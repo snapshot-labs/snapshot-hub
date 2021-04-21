30e75ec86ab926b9bcba31de70ce3f6f47beb3ed
Chaitanya <yourchaitu@gmail.com>
2021-04-21 21:22:17 +0530

    Call getProfiles only when profile data is requested

--- server/graphql/index.ts
+++ server/graphql/index.ts
@@ -1,68 +1,82 @@
-import { importSchema } from 'graphql-import';
-import { buildSchema } from 'graphql';
+import path from "path";
+import fs from "fs";
+import { makeExecutableSchema } from "graphql-tools";
+import graphqlFields from 'graphql-fields';
 import { spaces as registrySpaces } from '../helpers/spaces';
 import db from '../helpers/mysql';
 import { clone, jsonParse } from '../helpers/utils';
 import { getProfiles } from '../helpers/profile';
 
-const schemaFile = importSchema('./**/*.graphql');
-export const schema = buildSchema(schemaFile);
+const schemaFile = path.join(__dirname, "./schema.graphql");
+const typeDefs = fs.readFileSync(schemaFile, "utf8");
 
 export const rootValue = {
-  timeline: async ({ spaces = [], first = 10, skip = 0, state }) => {
-    const ts = parseInt((Date.now() / 1e3).toFixed());
-    if (spaces.length === 0) spaces = Object.keys(registrySpaces) as any;
+  Query: {
+    timeline: async ({ spaces = [], first = 10, skip = 0, state }, args, context, info) => {
+      const requestedFields = graphqlFields(info);
+      const ts = parseInt((Date.now() / 1e3).toFixed());
+      if (spaces.length === 0) spaces = Object.keys(registrySpaces) as any;
+  
+      const params: any[] = [1614473607, spaces];
+  
+      let stateStr = '';
+      if (state === 'pending') {
+        stateStr = 'AND JSON_EXTRACT(payload, "$.start") > ?';
+        params.push(ts);
+      }
+      if (state === 'active') {
+        stateStr =
+          'AND JSON_EXTRACT(payload, "$.start") < ? AND JSON_EXTRACT(payload, "$.end") > ?';
+        params.push(ts, ts);
+      }
+      if (state === 'closed') {
+        stateStr = 'AND ? > JSON_EXTRACT(payload, "$.end")';
+        params.push(ts);
+      }
+  
+      params.push(skip, first);
+  
+      const query = `SELECT * FROM messages WHERE type = 'proposal' AND timestamp > ? AND space IN (?) ${stateStr} ORDER BY timestamp DESC LIMIT ?, ?`;
+      const msgs = await db.queryAsync(query, params);
+  
+      const authors = Array.from(new Set(msgs.map(msg => msg.address)));
 
-    const params: any[] = [1614473607, spaces];
+      let users = {};
+      if(requestedFields.author && requestedFields.author.profile) {
+        users = await getProfiles(authors);
+      }
 
-    let stateStr = '';
-    if (state === 'pending') {
-      stateStr = 'AND JSON_EXTRACT(payload, "$.start") > ?';
-      params.push(ts);
+      return msgs.map(msg => {
+        const payload = jsonParse(msg.payload);
+        const { start, end } = payload;
+        let proposalState = 'pending';
+        if (ts > start) proposalState = 'active';
+        if (ts > end) proposalState = 'closed';
+  
+        const space = clone(registrySpaces[msg.space]);
+        space.id = msg.space;
+        space.private = space.private || false;
+        space.about = space.about || '';
+        space.members = space.members || [];
+  
+        return {
+          id: msg.id,
+          author: {
+            address: msg.address,
+            profile: users[msg.address]
+          },
+          timestamp: msg.timestamp,
+          state: proposalState,
+          start,
+          end,
+          snapshot: payload.snapshot,
+          name: payload.name,
+          body: payload.body,
+          space
+        };
+      });
     }
-    if (state === 'active') {
-      stateStr =
-        'AND JSON_EXTRACT(payload, "$.start") < ? AND JSON_EXTRACT(payload, "$.end") > ?';
-      params.push(ts, ts);
-    }
-    if (state === 'closed') {
-      stateStr = 'AND ? > JSON_EXTRACT(payload, "$.end")';
-      params.push(ts);
-    }
-
-    params.push(skip, first);
-
-    const query = `SELECT * FROM messages WHERE type = 'proposal' AND timestamp > ? AND space IN (?) ${stateStr} ORDER BY timestamp DESC LIMIT ?, ?`;
-    const msgs = await db.queryAsync(query, params);
-
-    const authors = Array.from(new Set(msgs.map(msg => msg.address)));
-    const users = await getProfiles(authors);
-
-    return msgs.map(msg => {
-      const payload = jsonParse(msg.payload);
-      const { start, end } = payload;
-      let proposalState = 'pending';
-      if (ts > start) proposalState = 'active';
-      if (ts > end) proposalState = 'closed';
-
-      const space = clone(registrySpaces[msg.space]);
-      space.id = msg.space;
-      space.private = space.private || false;
-      space.about = space.about || '';
-      space.members = space.members || [];
-
-      return {
-        id: msg.id,
-        author: users[msg.address],
-        timestamp: msg.timestamp,
-        state: proposalState,
-        start,
-        end,
-        snapshot: payload.snapshot,
-        name: payload.name,
-        body: payload.body,
-        space
-      };
-    });
   }
 };
+
+export const schema = makeExecutableSchema({ typeDefs, resolvers: rootValue });
