f19ac8b5be0104a351f16fff2e68297fa510cf59
bonustrack <fabien@bonustrack.co>
2021-04-30 14:37:41 +0700

    Increase default connection limit

--- server/helpers/mysql.ts
+++ server/helpers/mysql.ts
@@ -2,11 +2,11 @@ import mysql from 'mysql';
 import Pool from 'mysql/lib/Pool';
 import Connection from 'mysql/lib/Connection';
 import bluebird from 'bluebird';
+// @ts-ignore
 import parse from 'connection-string';
 
-// @ts-ignore
 const config = parse(process.env.DATABASE_URL);
-config.connectionLimit = 75;
+config.connectionLimit = process.env.CONNECTION_LIMIT || 150;
 config.multipleStatements = true;
 config.database = config.path[0];
 config.host = config.hosts[0].name;
