af55389dd30d744480c188a0a186384d59430d9a
bonustrack <fabien@bonustrack.co>
2021-05-08 03:06:38 +0700

    Use proposals table for GraphQL

--- server/graphql/index.ts
+++ server/graphql/index.ts
@@ -6,6 +6,7 @@ import { spaces as registrySpaces } from '../helpers/spaces';
 import db from '../helpers/mysql';
 import { clone, jsonParse } from '../helpers/utils';
 import { getProfiles } from '../helpers/profile';
+import { formatProposal } from './helpers';
 
 const schemaFile = path.join(__dirname, './schema.gql');
 const typeDefs = fs.readFileSync(schemaFile, 'utf8');
@@ -26,14 +27,11 @@ export const rootValue = {
       context,
       info
     ) => {
-      console.log('GraphQL request', first, skip, state);
-
       const requestedFields = graphqlFields(info);
-
       const ts = parseInt((Date.now() / 1e3).toFixed());
+
       const spaceKeys: string[] = Object.keys(registrySpaces);
       spaces = spaces.filter(space => spaceKeys.includes(space));
-      if (spaces.length === 0) spaces = spaceKeys as any;
 
       let queryStr = '';
       const params: any[] = [spaces];
@@ -97,6 +95,86 @@ export const rootValue = {
 
         return proposal;
       });
+    },
+
+    proposal: async (parent, { id }) => {
+      const query = `
+        SELECT p.*, spaces.settings FROM proposals p
+        INNER JOIN spaces ON spaces.id = p.space
+        WHERE p.id = ? AND spaces.settings IS NOT NULL
+        LIMIT 1
+      `;
+      try {
+        const proposals = await db.queryAsync(query, [id]);
+        return proposals.map(proposal => formatProposal(proposal))[0] || [];
+      } catch (e) {
+        console.log(e);
+        return Promise.reject('request failed');
+      }
+    },
+
+    proposals: async (parent, args, context, info) => {
+      const requestedFields = graphqlFields(info);
+      console.log(JSON.stringify(requestedFields));
+
+      const { where = {} } = args;
+      const ts = parseInt((Date.now() / 1e3).toFixed());
+      let queryStr = '';
+      const params: any[] = [];
+
+      const spaceIn = where.space_in || [];
+      if (spaceIn.length > 0) {
+        queryStr += `AND p.space IN (?) `;
+        params.push(spaceIn);
+      }
+
+      const id = where.id || null;
+      if (id) {
+        queryStr += `AND p.id = ? `;
+        params.push(id);
+      }
+
+      const idIn = where.id_in || [];
+      if (idIn.length > 0) {
+        queryStr += `AND p.id IN (?)`;
+        params.push(idIn);
+      }
+
+      const state = where.state || null;
+      if (state === 'pending') {
+        queryStr += 'AND p.start > ? ';
+        params.push(ts);
+      } else if (state === 'active') {
+        queryStr += 'AND p.start < ? AND p.end > ? ';
+        params.push(ts, ts);
+      } else if (state === 'closed') {
+        queryStr += 'AND p.end < ? ';
+        params.push(ts);
+      }
+
+      let orderBy = args.orderBy || 'created';
+      let orderDirection = args.orderDirection || 'desc';
+      if (!['created', 'start', 'end'].includes(orderBy)) orderBy = 'created';
+      orderBy = `p.${orderBy}`;
+      orderDirection = orderDirection.toUpperCase();
+      if (!['ASC', 'DESC'].includes(orderDirection)) orderDirection = 'DESC';
+
+      const { first = 20, skip = 0 } = args;
+      params.push(skip, first);
+
+      const query = `
+        SELECT p.*, spaces.settings FROM proposals p
+        INNER JOIN spaces ON spaces.id = p.space
+        WHERE spaces.settings IS NOT NULL ${queryStr}
+        ORDER BY ${orderBy} ${orderDirection} LIMIT ?, ?
+      `;
+      try {
+        const proposals = await db.queryAsync(query, params);
+        return proposals.map(proposal => formatProposal(proposal));
+      } catch (e) {
+        console.log(e);
+        return Promise.reject('request failed');
+      }
     }
   }
 };
