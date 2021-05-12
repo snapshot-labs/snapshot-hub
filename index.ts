9a8a7d47547f9a73e78ed85664b63b30915940a9
Chaitanya <yourchaitu@gmail.com>
2021-05-12 16:50:24 +0530

    Remove vue-cli (#75)
    * Remove vue-cli
    
    * Change script name
    
    * Node version fix
    
    * Fix start script
    
    * move ts-node from dev dependencies
    
    * Remove all vue related packages for eslint
    
    Co-authored-by: bonustrack <fabien@bonustrack.co>

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import { graphqlHTTP } from 'express-graphql';
 import rateLimit from 'express-rate-limit';
 import { createHash } from 'crypto';
+import express from 'express';
 import api from './server';
 import { schema, rootValue } from './server/graphql';
 import defaultQuery from './server/graphql/examples';
@@ -11,28 +12,32 @@ import { sendError } from './server/helpers/utils';
 
 dotenv.config();
 
-export default app => {
-  app.use(bodyParser.json({ limit: '20mb' }));
-  app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
-  app.use(cors());
-  app.set('trust proxy', 1);
-  app.use(
-    rateLimit({
-      windowMs: 10 * 1e3,
-      max: 32,
-      handler: (req, res) => {
-        const id = createHash('sha256')
-          .update(req.ip)
-          .digest('hex');
-        console.log('Too many requests', id.slice(0, 7));
-        sendError(res, 'too many requests', 429);
-      }
-    })
-  );
-  app.use('/api', api);
-  app.use(
-    '/graphql',
-    graphqlHTTP({ schema, rootValue, graphiql: { defaultQuery } })
-  );
-  app.get('/*', (req, res) => res.redirect('/api'));
-};
+const app = express();
+app.use(bodyParser.json({ limit: '20mb' }));
+app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
+app.use(cors());
+app.set('trust proxy', 1);
+app.use(
+  rateLimit({
+    windowMs: 10 * 1e3,
+    max: 32,
+    handler: (req, res) => {
+      const id = createHash('sha256')
+        .update(req.ip)
+        .digest('hex');
+      console.log('Too many requests', id.slice(0, 7));
+      sendError(res, 'too many requests', 429);
+    }
+  })
+);
+app.use('/api', api);
+app.use(
+  '/graphql',
+  graphqlHTTP({ schema, rootValue, graphiql: { defaultQuery } })
+);
+app.get('/*', (req, res) => res.redirect('/api'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`⚡⚡ Snapshot hub is running: http://localhost:${PORT}`)
+);
--- server/helpers/relayer.ts
+++ server/helpers/relayer.ts
@@ -1,6 +1,6 @@
 import { Wallet } from '@ethersproject/wallet';
 
-const privateKey = process.env.RELAYER_PK;
+const privateKey = process.env.RELAYER_PK || '';
 const wallet = new Wallet(privateKey);
 
 export default wallet;
