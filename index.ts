6a0b4c38d91932755cead8aa3b7511db46a8fd07
Chaitanya <yourchaitu@gmail.com>
2021-05-12 16:11:06 +0530

    Remove all vue related packages for eslint

--- index.ts
+++ index.ts
@@ -37,7 +37,7 @@ app.use(
 );
 app.get('/*', (req, res) => res.redirect('/api'));
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
   console.log(`⚡⚡ Snapshot hub is running: http://localhost:${PORT}`)
 );
