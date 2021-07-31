6a49939798e34fa8f84834b94fe4a157c55f0e09
Fabien <bonustrack@users.noreply.github.com>
2021-07-31 18:12:21 +0700

    Add IPFS field (#91)
    * Remove check to content hash
    
    * Add IPFS field
    
    * Add IPFS field on GraphQL

--- src/helpers/database/schema.sql
+++ src/helpers/database/schema.sql
@@ -12,11 +12,11 @@ CREATE TABLE hubs (
 
 CREATE TABLE messages (
   id VARCHAR(64) NOT NULL,
+  ipfs VARCHAR(64) NOT NULL,
   address VARCHAR(64) NOT NULL,
   version VARCHAR(6) NOT NULL,
   timestamp BIGINT NOT NULL,
   space VARCHAR(64),
-  token VARCHAR(64),
   type VARCHAR(12) NOT NULL,
   payload JSON,
   sig VARCHAR(256) NOT NULL,
@@ -42,6 +42,7 @@ CREATE TABLE spaces (
 
 CREATE TABLE proposals (
   id VARCHAR(64) NOT NULL,
+  ipfs VARCHAR(64) NOT NULL,
   author VARCHAR(64) NOT NULL,
   created INT(11) NOT NULL,
   space VARCHAR(64) NOT NULL,
@@ -66,6 +67,7 @@ CREATE TABLE proposals (
 
 CREATE TABLE votes (
   id VARCHAR(64) NOT NULL,
+  ipfs VARCHAR(64) NOT NULL,
   voter VARCHAR(64) NOT NULL,
   created INT(11) NOT NULL,
   space VARCHAR(64) NOT NULL,
@@ -91,6 +93,7 @@ CREATE TABLE events (
 
 CREATE TABLE follows (
   id VARCHAR(64) NOT NULL,
+  ipfs VARCHAR(64) NOT NULL,
   follower VARCHAR(64) NOT NULL,
   space VARCHAR(64) NOT NULL,
   created INT(11) NOT NULL,
