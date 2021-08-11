6c24f60ae543eaa463b37308cee15b5d2f63409a
bonustrack <fabien@bonustrack.co>
2021-08-12 01:20:15 +0700

    Fix writer follow / unfollow

--- src/writer/index.ts
+++ src/writer/index.ts
@@ -2,10 +2,14 @@ import * as proposal from './proposal';
 import * as vote from './vote';
 import * as settings from './settings';
 import * as deleteProposal from './delete-proposal';
+import * as follow from './follow';
+import * as unfollow from './unfollow';
 
 export default {
   proposal,
   vote,
   settings,
-  'delete-proposal': deleteProposal
+  'delete-proposal': deleteProposal,
+  follow,
+  unfollow
 };
