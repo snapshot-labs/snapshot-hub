d58dff31089ac97ef91f2a87192d12a26b1fc7e5
Fabien <bonustrack@users.noreply.github.com>
2021-08-12 19:55:03 +0700

    Add aliases (#95)
    * Fix writer follow / unfollow
    
    * Add follow GraphQL operation
    
    * Init aliases
    
    * Add writer

---
+++ src/writer/alias.ts
@@ -0,0 +1,18 @@
+import { getAddress } from '@ethersproject/address';
+import db from '../helpers/mysql';
+
+export async function verify(): Promise<any> {
+  return true;
+}
+
+export async function action(message, id): Promise<void> {
+  const params = {
+    id,
+    ipfs: id,
+    address: getAddress(message.from),
+    alias: getAddress(message.alias),
+    created: message.timestamp
+  };
+  await db.queryAsync('INSERT IGNORE INTO aliases SET ?', params);
+  console.log(`Stored: ${message.from} alias ${message.alias}`);
+}
