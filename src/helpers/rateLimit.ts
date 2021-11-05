3c3cf7bc47283a8a2aab3ad71429be4567cbcde3
Fabien <bonustrack@users.noreply.github.com>
2021-11-05 15:47:27 +0700

    Update rateLimit.ts

--- src/helpers/rateLimit.ts
+++ src/helpers/rateLimit.ts
@@ -2,8 +2,8 @@ import rateLimit from 'express-rate-limit';
 import { sendError, sha256 } from './utils';
 
 export default rateLimit({
-  windowMs: 10 * 1e3,
-  max: 64,
+  windowMs: 12 * 1e3,
+  max: 32,
   handler: (req, res) => {
     const id = sha256(req.ip);
     console.log('Too many requests', id.slice(0, 7));
