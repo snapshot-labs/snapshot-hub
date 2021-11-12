6e78f7fff8982b38b7f44590ee7d76fa5f1c9753
mktcode <kontakt@markus-kottlaender.de>
2021-11-12 02:26:01 +0100

    simplify votes graphql db query

--- src/graphql/operations/votes.ts
+++ src/graphql/operations/votes.ts
@@ -48,11 +48,7 @@ export default async function(parent, args, context?, info?) {
   let votes: any[] = [];
 
   const query = `
-    SELECT v.* FROM votes v
-    LEFT OUTER JOIN votes v2 ON
-      v.voter = v2.voter AND v.proposal = v2.proposal
-      AND ((v.created < v2.created) OR (v.created = v2.created AND v.id < v2.id))
-    WHERE v2.voter IS NULL ${queryStr}
+    SELECT * FROM votes v WHERE 1=1 ${queryStr}
     ORDER BY ${orderBy} ${orderDirection} LIMIT ?, ?
   `;
   try {
