c325d32cc7ecf15d541a5b43e5173e214d4a8dff
mktcode <kontakt@markus-kottlaender.de>
2021-11-19 23:35:55 +0100

    set base path via env var

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,18 @@ import graphql from './graphql';
 import rateLimit from './helpers/rateLimit';
 import './events';
 
+const PORT = process.env.PORT || 3000;
+const BASE_PATH = process.env.BASE_PATH || '';
+
 const app = express();
 app.use(bodyParser.json({ limit: '20mb' }));
 app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
 app.use(cors({ maxAge: 86400 }));
 app.set('trust proxy', 1);
 app.use(rateLimit);
-app.use('/api', api);
-app.use('/api', upload);
-app.use('/graphql', graphql);
-app.get('/*', (req, res) => res.redirect('/api'));
+app.use(BASE_PATH + '/api', api);
+app.use(BASE_PATH + '/api', upload);
+app.use(BASE_PATH + '/graphql', graphql);
+app.get(BASE_PATH + '/*', (req, res) => res.redirect('/api'));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Started on: http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Started on: http://localhost:${PORT}${BASE_PATH}`));
