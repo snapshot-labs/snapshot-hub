ec52144f0c69cf06f3be2086dfd98f53b20c0755
Fabien <bonustrack@users.noreply.github.com>
2021-12-12 17:42:13 +0700

    Update rateLimit.ts

--- src/helpers/rateLimit.ts
+++ src/helpers/rateLimit.ts
@@ -3,7 +3,7 @@ import { sendError, sha256 } from './utils';
 
 export default rateLimit({
   windowMs: 12 * 1e3,
-  max: 24,
+  max: 32,
   handler: (req, res) => {
     const id = sha256(req.ip);
     console.log('Too many requests', id.slice(0, 7));
