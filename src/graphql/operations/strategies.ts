9bcc879f90b03065a4255af2f511bc8c7ed9d58a
Fabien <bonustrack@users.noreply.github.com>
2022-01-28 19:41:34 +0700

    Expose strategies from Score API (#243)

--- src/graphql/operations/strategies.ts
+++ src/graphql/operations/strategies.ts
@@ -1,19 +1,5 @@
-import { spaces } from '../../helpers/spaces';
+import { strategies } from '../../helpers/strategies';
 
 export default function() {
-  const strategies = {};
-  Object.values(spaces).forEach((space: any) => {
-    const uniqueStrategies = new Set<string>(
-      space.strategies.map(strategy => strategy.name)
-    );
-    uniqueStrategies.forEach(strategyName => {
-      strategies[strategyName] = strategies[strategyName]
-        ? strategies[strategyName] + 1
-        : 1;
-    });
-  });
-  return Object.entries(strategies).map(strategy => ({
-    id: strategy[0],
-    spacesCount: strategy[1]
-  }));
+  return strategies;
 }
