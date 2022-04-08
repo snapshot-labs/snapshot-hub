97b3b91332b4a3757586b70713113e417437e6dd
mkt <kontakt@markus-kottlaender.de>
2022-04-08 17:51:25 +0200

    Update rateLimit.ts

--- src/helpers/rateLimit.ts
+++ src/helpers/rateLimit.ts
@@ -3,7 +3,7 @@ import { sendError, sha256 } from './utils';
 
 export default rateLimit({
   windowMs: 16 * 1e3,
-  max: 64,
+  max: 128,
   handler: (req, res) => {
     const id = sha256(req.ip);
     console.log('Too many requests', id.slice(0, 7));
