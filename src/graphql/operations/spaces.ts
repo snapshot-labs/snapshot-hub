9bc0ad4b39b743883a98ebeefd57ca63036ea12f
mktcode <kontakt@markus-kottlaender.de>
2022-04-10 11:24:33 +0200

    use only one query to fetch related spaces and return only matching child/parent relationships

--- src/graphql/operations/spaces.ts
+++ src/graphql/operations/spaces.ts
@@ -3,50 +3,53 @@ import { fetchSpaces } from '../helpers';
 
 export default async function(_parent, args, _context, info) {
   try {
-    // maximum sum of parents and children to fetch
-    // only used if parent or children fields other than id are requested
-    const maxSubRequests = 100;
     const requestedFields = info ? graphqlFields(info) : {};
+    let spaces = await fetchSpaces(args);
 
-    const spaces = await fetchSpaces(args);
-
+    // fetch parent and child spaces if more than their id is requested
     if (
       (requestedFields.parent &&
         Object.keys(requestedFields.parent).some(key => key !== 'id')) ||
       (requestedFields.children &&
         Object.keys(requestedFields.children).some(key => key !== 'id'))
     ) {
-      // count all parents and children in the returned spaces list, to make sure we don't exceed the max
-      // otherwise throw explanatory error
-      const subRequestsToMake = spaces.reduce((a, space) => {
-        if (space.children) a += space.children.length;
-        if (space.parent) a++;
-        return a;
-      }, 0);
-
-      if (subRequestsToMake > maxSubRequests) {
-        throw new Error(
-          `Too many subrequests to make. (Max: ${maxSubRequests}) Query only 'id' for parent and children nodes or reduce result limit.`
+      // throw error if anything other than id is requested on second level
+      // (e.g. children.parent.name or parent.children.name)
+      if (
+        (requestedFields.parent?.children &&
+          Object.keys(requestedFields.parent.children).some(key => key !== 'id')) ||
+        (requestedFields.children?.parent &&
+          Object.keys(requestedFields.children.parent).some(key => key !== 'id'))
+      ) {
+        return new Error(
+          'Unsupported nested fields for related spaces. Only id is supported for children\'s parent or parent\'s children.'
         );
       }
 
-      // fetch parents and children
-      for (const space of spaces) {
-        if (space.parent?.id) {
-          const parentResult = await fetchSpaces({
-            where: { id: space.parent.id }
-          });
-          if (parentResult.length === 1) {
-            space.parent = parentResult[0];
-          }
-        }
+      // collect all parent and child ids of all returned spaces
+      const relatedSpaceIDs = spaces.reduce((ids, space) => {
+        if (space.children) ids.push(...space.children.map(c => c.id));
+        if (space.parent) ids.push(space.parent.id);
+        return ids;
+      }, []);
 
-        if (space.children?.length) {
-          space.children = await fetchSpaces({
-            where: { id_in: space.children.map(c => c.id) }
-          });
+      // fetch all related spaces
+      const relatedSpaces = await fetchSpaces({
+        where: { id_in: relatedSpaceIDs }
+      });
+
+      // map related spaces to main list of spaces (where parent and child match)
+      spaces = spaces.map(space => {
+        if (space.children) {
+          space.children = space.children
+            .map(c => relatedSpaces.find(s => s.id === c.id && s.parent && s.parent.id === space.id))
+            .filter(s => s);
         }
-      }
+        if (space.parent) {
+          space.parent = relatedSpaces.find(s => s.id === space.parent.id && s.children.map(c => c.id).includes(space.id));
+        }
+        return space;
+      });
     }
 
     return spaces;
