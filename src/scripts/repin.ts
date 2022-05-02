c00623532b15f2ab48947e717caf5f434192fd25
mktcode <kontakt@markus-kottlaender.de>
2022-05-02 12:53:41 +0200

    implemented vote repinning

--- src/scripts/repin.ts
+++ src/scripts/repin.ts
@@ -8,46 +8,35 @@
 import 'dotenv/config';
 import db from '../helpers/mysql';
 import { pin } from '@snapshot-labs/pineapple';
+import IPFSHash from 'ipfs-only-hash';
 
-// import { proposalTypes } from '@snapshot-labs/snapshot.js/src/sign/types';
-// doesn't work because it returns the json file not the ts file
-const proposalTypes = {
-  Proposal: [
-    { name: 'from', type: 'address' },
-    { name: 'space', type: 'string' },
-    { name: 'timestamp', type: 'uint64' },
-    { name: 'type', type: 'string' },
-    { name: 'title', type: 'string' },
-    { name: 'body', type: 'string' },
-    { name: 'discussion', type: 'string' },
-    { name: 'choices', type: 'string[]' },
-    { name: 'start', type: 'uint64' },
-    { name: 'end', type: 'uint64' },
-    { name: 'snapshot', type: 'uint64' },
-    { name: 'network', type: 'string' },
-    { name: 'strategies', type: 'string' },
-    { name: 'plugins', type: 'string' },
-    { name: 'metadata', type: 'string' }
-  ]
-};
+import {
+  proposalTypes,
+  voteTypes,
+  voteArrayTypes,
+  voteStringTypes,
+  vote2Types,
+  voteArray2Types,
+  voteString2Types
+// } from '@snapshot-labs/snapshot.js/src/sign/types';
+} from './types';
+// import from snapshot.js doesn't work, because it returns the json file not
+// the ts file, and ts extension doesn't work either, idk why. Put a copy of
+// the file in the same directory as this script.
 
-// take proposal id as parameter
-const proposalId = process.argv[2];
-if (!proposalId) {
-  console.log('Usage: yarn run scripts:repin <proposal id>');
-  process.exit(1);
-}
+(async () => {
+  // take proposal id as parameter and load proposal from db
+  const proposalId = process.argv[2];
+  if (!proposalId) {
+    console.log('Usage: yarn run scripts:repin <proposal id>');
+    process.exit(1);
+  }
 
-let proposalsRepinned = 0;
-let votesRepinned = 0;
-let receiptsRepinned = 0;
-
-const run = async () => {
   const proposalQuery = `SELECT
       p.*,
-      m.sig as sig,
-      m.version as version,
-      m.timestamp as timestamp
+      m.sig as msgSig,
+      m.version as msgVersion,
+      m.timestamp as msgTimestamp
     FROM
       proposals p,
       messages m
@@ -56,12 +45,15 @@ const run = async () => {
   const proposals = await db.queryAsync(proposalQuery, [proposalId]);
 
   if (proposals.length !== 1) {
-    console.log(`Proposal ${proposalId} not found`);
+    console.log(`Proposal ${proposalId} not found.`);
     process.exit(1);
   }
 
-  // repin proposal
   const proposal = proposals[0];
+
+  let proposalsRepinned = 0;
+  let votesRepinned = 0;
+  // let receiptsRepinned = 0;
 
   // sort strategy properties
   // strategies are passed in proposal create messages but what's stored in the db
@@ -71,14 +63,15 @@ const run = async () => {
     return Object.keys(strategy).sort((a, b) => a.localeCompare(b))
       .reduce((acc, key) => { acc[key] = strategy[key]; return acc; }, {});
   }));
-  // get message
+
+  // rebuild message
   const proposalMessage = {
     address: proposal.author,
-    sig: proposal.sig,
+    sig: proposal.msgSig,
     data: {
       domain: {
         name: 'snapshot',
-        version: proposal.version
+        version: proposal.msgVersion
       },
       types: proposalTypes,
       message: {
@@ -96,22 +89,85 @@ const run = async () => {
         plugins: proposal.plugins,
         metadata: '{}',
         from: proposal.author,
-        timestamp: proposal.timestamp
+        timestamp: proposal.msgTimestamp
       }
     }
   }
 
-  const ipfsHash = await pin(proposalMessage);
-  if (ipfsHash.cid === proposal.ipfs) proposalsRepinned++;
+  const rebuiltMessageHash = await IPFSHash.of(JSON.stringify(proposalMessage));
+  
+  if (rebuiltMessageHash === proposal.ipfs) {
+    await pin(proposalMessage);
+    proposalsRepinned++;
+    console.log(`Proposal ${proposalId} repinned. Continuing with votes.`);
+  } else {
+    console.error(`Rebuilt message for proposal ${proposalId} has an unexpected IPFS hash.`);
+    process.exit(1);
+  }
 
-  const votesQuery = `SELECT * FROM votes WHERE proposal = ?`;
+  // fetch and repin votes
+  const votesQuery = `SELECT
+      v.*,
+      m.sig as msgSig,
+      m.version as msgVersion,
+      m.timestamp as msgTimestamp
+    FROM
+      votes v,
+      messages m
+    WHERE
+      v.proposal = ? AND m.id = v.id`;
   const votes = await db.queryAsync(votesQuery, [proposalId]);
+
+  const isType2Proposal = proposalId.startsWith('0x');
+
+  for (const vote of votes) {
+    // select the correct type for the vote, based on proposal type and voting type
+    let type = isType2Proposal ? vote2Types : voteTypes;
+    let choice = JSON.parse(vote.choice);
+    if (['approval', 'ranked-choice'].includes(proposal.type)) {
+      type = isType2Proposal ? voteArray2Types : voteArrayTypes;
+    }
+    if (['quadratic', 'weighted'].includes(proposal.type)) {
+      type = isType2Proposal ? voteString2Types : voteStringTypes;
+      choice = JSON.stringify(choice);
+    }
+
+    // rebuild message
+    const voteMessage = {
+      address: vote.voter,
+      sig: vote.msgSig,
+      data: {
+        domain: {
+          name: 'snapshot',
+          version: vote.msgVersion
+        },
+        types: type,
+        message: {
+          space: vote.space,
+          proposal: vote.proposal,
+          choice: choice,
+          metadata: vote.metadata,
+          from: vote.voter,
+          timestamp: vote.msgTimestamp
+        }
+      }
+    }
+
+    const rebuiltMessageHash = await IPFSHash.of(JSON.stringify(voteMessage));
+
+    if (rebuiltMessageHash === vote.ipfs) {
+      await pin(voteMessage);
+      votesRepinned++;
+      console.log(`Vote ${vote.ipfs} repinned.`);
+    } else {
+      console.error(`Rebuilt message for vote ${vote.ipfs} has an unexpected IPFS hash.`);
+      process.exit(1);
+    }
+  }
 
   console.log(`${proposalsRepinned} proposals repinned.`);
   console.log(`${votesRepinned} votes repinned.`);
-  console.log(`${receiptsRepinned} receipts repinned.`);
+  // console.log(`${receiptsRepinned} receipts repinned.`);
   
   process.exit(0);
-};
-
-run();
+})();
