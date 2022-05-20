64684cb0da66bf6c4c4b1459f231b07e49156ae7
mktcode <kontakt@markus-kottlaender.de>
2022-05-20 22:59:27 +0200

    added domain argument for space

--- src/graphql/operations/space.ts
+++ src/graphql/operations/space.ts
@@ -1,15 +1,18 @@
 import db from '../../helpers/mysql';
 import { formatSpace } from '../helpers';
 
-export default async function(_parent, { id }) {
+export default async function (_parent, { id, domain }) {
+  if (id && domain) {
+    throw new Error('Identifier can either be id or domain but not both.');
+  }
   const query = `
     SELECT s.* FROM spaces s
-    WHERE s.id = ? AND s.settings IS NOT NULL
+    WHERE s.${domain ? 'domain' : 'id'} = ? AND s.settings IS NOT NULL
     GROUP BY s.id
     LIMIT 1
   `;
   try {
-    const spaces = await db.queryAsync(query, [id]);
+    const spaces = await db.queryAsync(query, [id || domain]);
     return (
       spaces.map(space =>
         Object.assign(space, formatSpace(space.id, space.settings))
