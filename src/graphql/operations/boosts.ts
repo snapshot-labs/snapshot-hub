04fdda08c2ed00e815b4efec158cb999d4565fdc
mktcode <kontakt@markus-kottlaender.de>
2022-06-22 16:04:47 +0200

    fixed boosts resolver

--- src/graphql/operations/boosts.ts
+++ src/graphql/operations/boosts.ts
@@ -1,17 +1,34 @@
+import fetch from 'cross-fetch';
 import graphqlFields from 'graphql-fields';
-import { querySubgraph } from '@snapshot-labs/boost';
+
+async function querySubgraph(
+  query: string,
+  chainId: number,
+  apiKey: string
+) {
+  const apiUrls: string[] = [];
+  apiUrls[4] = `https://api.studio.thegraph.com/query/12054/boost/0.1.1`;
+
+  return await fetch(apiUrls[chainId], {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Api-Key': apiKey
+    },
+    body: JSON.stringify({ query })
+  }).then(res => res.json());
+}
 
 export default async function (_parent, { ref }, _context, info) {
   const requestedFields = info ? graphqlFields(info) : {};
 
   try {
-    const query = `
-      query {
-        boosts (where: { ref: "${ref}" }) {
-          ${Object.keys(requestedFields).join('\n')}
-        }
+    const query = `{
+      boosts (where: { ref: "${ref}" }) {
+        ${Object.keys(requestedFields).join('\n')}
       }
-    `;
+    }`;
+
     const response = await querySubgraph(query, 4, process.env.BOOST_SUBGRAPH_API_KEY as string);
     return response?.data?.boosts || [];
   } catch (e) {
