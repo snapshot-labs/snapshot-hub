9a4a8fc61c8d31abeed1be95a4fabed838254b64
bonustrack <fabien@bonustrack.co>
2022-08-26 00:54:11 +0700

    Init Shutter

---
+++ src/helpers/shutter.ts
@@ -0,0 +1,41 @@
+import fetch from 'cross-fetch';
+import * as shutter from '@shutter-network/shutter-crypto';
+import { arrayify } from '@ethersproject/bytes';
+import { toUtf8String, formatBytes32String } from '@ethersproject/strings';
+
+const SHUTTER_URL = 'https://preview.snapshot.shutter.network/api/v1/rpc';
+// const SHUTTER_EON_PUBKEY = process.env.SHUTTER_EON_PUBKEY;
+
+export async function getDecryptionKey(proposal: string, url: string = SHUTTER_URL) {
+  const isByte32 = proposal.startsWith('0x');
+  const id = (isByte32 ? proposal : formatBytes32String(proposal)).slice(2);
+  const params = {
+    eon_id: 0,
+    proposal: id
+  };
+  const init = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      jsonrpc: '2.0',
+      method: 'get_decryption_key',
+      params,
+      id: null
+    })
+  };
+  const res = await fetch(url, init);
+  return (await res.json()).result;
+}
+
+export async function decrypt(encryptedMsg, decryptionKey) {
+  await shutter.init();
+  // const decryptionKey = '0x219BA688C8505178E50E7E4FEAEFA21BDA69172E71B980A365F6F873DC9B3AAA20076B6D92CB58B24D14B70789A0B37418A0508624C83A7C8E35ED0A8DBB0E4B';
+  const decryptionKeyArr = arrayify(decryptionKey);
+  // const encryptedMsg = '0x1c894825b72a15d6a0d5333270bf82800d8acb294fc1316f2caa549263f4ceb820c42da9d6d4a89c06de78b0c8726b83f28630dffa96cbac199dfef42eb72f4a08b9eea2e8283b34aaddf6c64ba6e8480e0577f6c7ea0e235f2083bed802836f29c69d21eac847cf0330ccefba1bc75c3a954e7242e0812d4399171fd7547d4285efd11438cdc5fd9e19aa143aae1f076d3b71a1971b578226f37ea2950899f4c07a186fdfe4bee88669179317c00383c8b20a8d8a96ad581baf161dd0673ec2459b6ad5cfec7d24cf0ce4a55a88b66858503e2a37af71544222cc8482c9af65';
+  const encryptedMsgArr = arrayify(encryptedMsg);
+  const decryptedMsg = await shutter.decrypt(encryptedMsgArr, decryptionKeyArr);
+  return toUtf8String(decryptedMsg);
+}
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import ingestor from './ingestor';
 import graphql from './graphql';
 import rateLimit from './helpers/rateLimit';
 import './helpers/strategies';
+import { getDecryptionKey } from './helpers/shutter';
 
 const app = express();
 app.use(express.json({ limit: '20mb' }));
@@ -20,3 +21,15 @@ app.get('/*', (req, res) => res.redirect('/api'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Started on: http://localhost:${PORT}`));
+
+async function test() {
+  try {
+    const proposal = '0x480184f2b2dedec2641fb1a0b8cb1f0a8af8e7edd90f2f5acfc0858c29ed964d';
+    const result = await getDecryptionKey(proposal);
+    console.log('Shutter', result);
+  } catch (e) {
+    console.log('Shutter', e);
+  }
+}
+
+test();
