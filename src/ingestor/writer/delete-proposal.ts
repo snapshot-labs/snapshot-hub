c6cd883ebd07927b82d3baa81ffa6a933edb8016
bonustrack <fabien@bonustrack.co>
2022-09-24 18:00:24 +0700

    Remove webhook related code

--- src/ingestor/writer/delete-proposal.ts
+++ src/ingestor/writer/delete-proposal.ts
@@ -1,7 +1,6 @@
 import { getProposal, getSpace } from '../../helpers/actions';
 import { jsonParse } from '../../helpers/utils';
 import db from '../../helpers/mysql';
-import { sendToWebhook } from '../../helpers/webhook';
 
 export async function verify(body): Promise<any> {
   const msg = jsonParse(body.msg);
@@ -17,19 +16,9 @@ export async function action(body): Promise<void> {
   const msg = jsonParse(body.msg);
   const id = msg.payload.proposal;
 
-  const ts = parseInt((Date.now() / 1e3).toFixed());
-
   const query = `
   DELETE FROM proposals WHERE id = ? LIMIT 1;
   DELETE FROM votes WHERE proposal = ?;
   `;
   await db.queryAsync(query, [id, id]);
-
-  const event = {
-    id: `proposal/${id}`,
-    space: msg.space,
-    event: 'proposal/deleted',
-    expire: ts
-  };
-  sendToWebhook(event);
 }
