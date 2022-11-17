ef518410e569565f9c9a53602dcf3d874d854d5f
Chaitanya <yourchaitu@gmail.com>
2022-11-17 12:19:57 +0530

    Use getJSON to ignore our IPNS (#511)
    * Poke space only if protocol is IPFS
    
    * Use getJSON
    
    * remove log
    
    * Unused variables

--- src/helpers/ens.ts
+++ src/helpers/ens.ts
@@ -1,28 +1,16 @@
-import fetch from 'cross-fetch';
 import snapshot from '@snapshot-labs/snapshot.js';
-import gateways from '@snapshot-labs/snapshot.js/src/gateways.json';
 import log from './log';
 
-const gateway = gateways[0];
 const DEFAULT_NETWORK = process.env.DEFAULT_NETWORK || '1';
-
-export async function uriGet(gateway: string, key: string, protocolType = 'ipfs') {
-  key = key.replace('storage.snapshot.page', 'storageapi.fleek.co/snapshot-team-bucket');
-  if (key.includes('storageapi.fleek.co')) protocolType = 'https';
-  let url = `https://${gateway}/${protocolType}/${key}`;
-  if (['https', 'http'].includes(protocolType)) url = `${protocolType}://${key}`;
-  return fetch(url).then(res => res.json());
-}
 
 export async function getSpaceENS(id) {
   let space = false;
   const uri: any = await snapshot.utils.getSpaceUri(id, DEFAULT_NETWORK);
   if (uri) {
     try {
-      const [protocolType, key] = uri.split('://');
-      space = await uriGet(gateway, key, protocolType);
+      space = await snapshot.utils.getJSON(uri);
     } catch (e) {
-      log.warn(`[ens] getSpace failed ${id}, ${JSON.stringify(e)}`);
+      log.warn(`[ens] getSpaceENS failed ${id}, ${JSON.stringify(e)}`);
     }
   }
   return space;
