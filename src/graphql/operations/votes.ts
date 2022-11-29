e586eed4f12d952b8a4d6a8ec1a4c94f21801587
bonustrack <fabien@bonustrack.co>
2022-11-29 20:17:16 +0700

    Change limit to 1000 for load votes

--- src/graphql/operations/votes.ts
+++ src/graphql/operations/votes.ts
@@ -4,7 +4,7 @@ import { buildWhereQuery, formatProposal, formatSpace, formatVote } from '../hel
 import serve from '../../helpers/ee';
 import log from '../../helpers/log';
 
-const LIMIT = 5000;
+const LIMIT = 1000;
 
 async function query(parent, args, context?, info?) {
   const requestedFields = info ? graphqlFields(info) : {};
