03bc9d794b3318628a3ecc7e465019eb084737ee
ChaituVR <yourchaitu@gmail.com>
2023-03-21 00:19:09 +0530

    Ignore deleted spaces

--- src/graphql/helpers.ts
+++ src/graphql/helpers.ts
@@ -146,7 +146,7 @@ export async function fetchSpaces(args) {
 
   const query = `
     SELECT s.* FROM spaces s
-    WHERE 1 = 1 ${queryStr}
+    WHERE 1 = 1 ${queryStr} AND s.deleted = 0
     GROUP BY s.id
     ORDER BY s.${orderBy} ${orderDirection} LIMIT ?, ?
   `;
