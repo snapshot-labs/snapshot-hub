980aca9fbfebe1f3540dfd370583eca8596c4eb8
Chaitanya <yourchaitu@gmail.com>
2023-05-18 00:47:25 +0530

    Update spaces.ts

--- src/helpers/spaces.ts
+++ src/helpers/spaces.ts
@@ -36,7 +36,7 @@ function getPopularity(
 
   if (!params.networks.some(network => testnets.includes(network))) popularity = 1;
   if (!params.strategies.some(strategy => testStrategies.includes(strategy))) popularity = 1;
-  if (params.verified) popularity *= 1000;
+  if (params.verified) popularity *= 100000;
 
   return popularity;
 }
