24c6742c8885d6872ef69ca71bcc2710b6884079
Chaitanya <yourchaitu@gmail.com>
2023-05-19 02:36:44 +0530

    Update src/helpers/keycard.ts

--- src/helpers/keycard.ts
+++ src/helpers/keycard.ts
@@ -4,7 +4,7 @@ import { sendError } from './utils';
 const keycard = new Keycard({
   app: 'snapshot-hub',
   secret: process.env.KEYCARD_SECRET || '',
-  URL: process.env.KEYCARD_URL || 'http://localhost:3007'
+  URL: process.env.KEYCARD_URL || 'https://keycard.snapshot.org'
 });
 
 const verifyKeyCard = async (req, res, next) => {
