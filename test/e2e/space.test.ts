3eb5af2120f809e907e786cc1f296f6ddd6a43ab
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-07-27 18:05:24 +0900

    chore: setup test env and add some basic test to implement

---
+++ test/e2e/space.test.ts
@@ -0,0 +1,9 @@
+describe('GET /api/space/:key', () => {
+  describe('when the space exists', () => {
+    it.todo('returns a space object');
+  });
+
+  describe('when the space does not exist', () => {
+    it.todo('returns a 404 error');
+  });
+});
