479669b40e639ac869f5375f53fdc3463490ebdf
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-07-27 13:07:37 +0900

    feat: setup sentry

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,13 @@ import log from './helpers/log';
 import './helpers/strategies';
 import { verifyKeyCard } from './helpers/keycard';
 import './helpers/moderation';
+import { initLogger, fallbackLogger } from './helpers/sentry';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+initLogger(app);
+
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ limit: '20mb', extended: false }));
 app.use(cors({ maxAge: 86400 }));
@@ -17,7 +22,8 @@ app.set('trust proxy', 1);
 app.use(rateLimit);
 app.use('/api', api);
 app.use('/graphql', verifyKeyCard, graphql);
+
+fallbackLogger(app);
 app.get('/*', (req, res) => res.redirect('/api'));
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => log.info(`Started on: http://localhost:${PORT}`));
