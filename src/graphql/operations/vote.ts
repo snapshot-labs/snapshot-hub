767f6154017a5c152a7541cb290adbf782a9f813
ChaituVR <yourchaitu@gmail.com>
2023-10-13 10:48:59 +0530

    Update vote and votes query

--- src/graphql/operations/vote.ts
+++ src/graphql/operations/vote.ts
@@ -18,7 +18,7 @@ export default async function (parent, { id }, context, info) {
     if (requestedFields.proposal && result?.proposal) {
       const proposalId = result.proposal;
       const query = `
-        SELECT p.*, spaces.settings FROM proposals p
+        SELECT p.*, spaces.settings, spaces.flagged as spaceFlagged, spaces.verified as spaceVerified FROM proposals p
         INNER JOIN spaces ON spaces.id = p.space
         WHERE spaces.settings IS NOT NULL AND p.id = ?
       `;
