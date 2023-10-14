4f5345ac274867f3a0db5d17766dd3dcbe907cc0
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-10-14 19:40:04 +0900

    chore: add dummy test

---
+++ test/integrations/graphql/operations/votes.test.ts
@@ -0,0 +1,7 @@
+describe('operatins/votes', () => {
+  describe('when filtering by choice', () => {
+    it.todo('returns votes for single choice');
+    it.todo('returns votes for multiple choice');
+    it.todo('returns votes for weighted');
+  });
+});
