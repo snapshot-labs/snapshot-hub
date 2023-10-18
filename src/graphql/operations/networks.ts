90a83e64f09759739dd63db759b450b585df412e
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-10-18 19:46:57 +0900

    fix: get `network` data from DB directly

--- src/graphql/operations/networks.ts
+++ src/graphql/operations/networks.ts
@@ -1,12 +1,20 @@
-import { spaces } from '../../helpers/spaces';
+import { capture } from '@snapshot-labs/snapshot-sentry';
+import db from '../../helpers/mysql';
 
-export default function () {
-  const networks = {};
-  Object.values(spaces).forEach((space: any) => {
-    networks[space.network] = networks[space.network] ? networks[space.network] + 1 : 1;
-  });
-  return Object.entries(networks).map(network => ({
-    id: network[0],
-    spacesCount: network[1]
-  }));
+export default async function () {
+  const query = `
+    SELECT
+      JSON_UNQUOTE(JSON_EXTRACT(settings, '$.network')) as network,
+      COUNT(name) AS spacesCount
+    FROM spaces
+    GROUP BY network
+    ORDER BY spacesCount DESC;
+  `;
+
+  try {
+    return (await db.queryAsync(query)).map(v => ({ ...v, id: v.network }));
+  } catch (e: any) {
+    capture(e);
+    return Promise.reject(new Error('request failed'));
+  }
 }
