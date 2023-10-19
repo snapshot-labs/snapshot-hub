0ca5df02402fd916ec2e0a3688aa4ce67539cad5
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-10-19 11:50:40 +0900

    feat: return fields from networks.json in network operation

--- src/graphql/operations/networks.ts
+++ src/graphql/operations/networks.ts
@@ -1,18 +1,28 @@
 import { capture } from '@snapshot-labs/snapshot-sentry';
+import networks from '@snapshot-labs/snapshot.js/src/networks.json';
 import db from '../../helpers/mysql';
 
 export default async function () {
   const query = `
     SELECT
       JSON_UNQUOTE(JSON_EXTRACT(settings, '$.network')) as network,
-      COUNT(name) AS spacesCount
+      COUNT(name) AS count
     FROM spaces
     GROUP BY network
-    ORDER BY spacesCount DESC;
+    ORDER BY count DESC;
   `;
 
   try {
-    return (await db.queryAsync(query)).map(v => ({ ...v, id: v.network }));
+    const results = new Map(await db.queryAsync(query).map(r => [r.network, r.count]));
+
+    return Object.values(networks)
+      .map((network: any) => {
+        network.id = network.key;
+        network.spacesCount = results.get(network.id) || 0;
+        network.testnet ||= false;
+        return network;
+      })
+      .sort((a, b) => b.spacesCount - a.spacesCount);
   } catch (e: any) {
     capture(e);
     return Promise.reject(new Error('request failed'));
