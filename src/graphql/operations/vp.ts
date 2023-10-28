19108cfe01ba13cb461e580aaa1f58f7848d6984
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-10-28 18:09:23 +0700

    fix: migrate to snapshot.js 0.8-beta with ofetch support

--- src/graphql/operations/vp.ts
+++ src/graphql/operations/vp.ts
@@ -1,12 +1,30 @@
 import snapshot from '@snapshot-labs/snapshot.js';
+import type { Strategy } from '@snapshot-labs/snapshot.js/dist/voting/types';
+import { GraphQLError } from 'graphql';
 import db from '../../helpers/mysql';
 
 const scoreAPIUrl = process.env.SCORE_API_URL || 'https://score.snapshot.org';
+
+type Payload = {
+  voter: string;
+  network?: string;
+  strategies?: Strategy[];
+  snapshot: number | 'latest';
+  space: string;
+  options: any;
+};
 
 export default async function (_parent, { voter, space, proposal }) {
   if (voter === '0x0000000000000000000000000000000000000000' || voter === '') {
     return Promise.reject(new Error('invalid address'));
   }
+
+  const payload: Payload = {
+    voter,
+    space,
+    snapshot: 'latest',
+    options: { url: scoreAPIUrl }
+  };
 
   if (proposal) {
     const query = `SELECT * FROM proposals WHERE id = ? LIMIT 1`;
@@ -16,15 +34,9 @@ export default async function (_parent, { voter, space, proposal }) {
       return Promise.reject(new Error('proposal not found'));
     }
 
-    return await snapshot.utils.getVp(
-      voter,
-      p.network,
-      JSON.parse(p.strategies),
-      p.snapshot,
-      space,
-      false,
-      { url: scoreAPIUrl }
-    );
+    payload.network = p.network;
+    payload.strategies = JSON.parse(p.strategies);
+    payload.snapshot = p.snapshot;
   } else if (space) {
     const query = `SELECT settings FROM spaces WHERE id = ? AND deleted = 0 LIMIT 1`;
     let [s] = await db.queryAsync(query, [space]);
@@ -34,11 +46,20 @@ export default async function (_parent, { voter, space, proposal }) {
     }
 
     s = JSON.parse(s.settings);
-
-    return await snapshot.utils.getVp(voter, s.network, s.strategies, 'latest', space, false, {
-      url: scoreAPIUrl
-    });
+    payload.network = s.network;
+    payload.strategies = s.strategies;
   }
 
-  return Promise.reject(new Error('missing argument'));
+  try {
+    return await snapshot.utils.getVp(
+      payload.voter,
+      payload.network as string,
+      payload.strategies as Strategy[],
+      payload.snapshot,
+      payload.space,
+      payload.options
+    );
+  } catch (e: any) {
+    throw new GraphQLError('failed to get voting power', null, null, null, null, null, e);
+  }
 }
