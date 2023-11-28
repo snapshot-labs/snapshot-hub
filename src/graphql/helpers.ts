110ba0c765f7475505f5818aa95d052b4a47737e
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-11-28 18:28:45 +0400

    chore: convert all throw error to `PublicError`

--- src/graphql/helpers.ts
+++ src/graphql/helpers.ts
@@ -36,10 +36,10 @@ export function checkLimits(args: any = {}, type) {
     const skipLimitReached = key === 'skip' && args[key] > limit;
     const whereLimitReached = key.endsWith('_in') ? where[key]?.length > limit : where[key] > limit;
     if (firstLimitReached || skipLimitReached || whereLimitReached)
-      throw new Error(`The \`${key}\` argument must not be greater than ${limit}`);
+      throw new PublicError(`The \`${key}\` argument must not be greater than ${limit}`);
 
     if (['first', 'skip'].includes(key) && args[key] < 0) {
-      throw new Error(`The \`${key}\` argument must be positive`);
+      throw new PublicError(`The \`${key}\` argument must be positive`);
     }
   }
   return true;
