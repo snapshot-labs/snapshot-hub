b383fa1ddd9f2b9b6a84d0df17d48d4bd246c9bd
Chaitanya <yourchaitu@gmail.com>
2023-12-23 18:00:00 +0530

    fix: SQL query to get space is missing turbo field (#776)
    * fix: SQL query to get space is missing turbo field
    
    * fix test case
    
    * fix test case

--- test/fixtures/spaces.ts
+++ test/fixtures/spaces.ts
@@ -4,6 +4,8 @@ const fixtures: Record<string, any>[] = [
     name: 'Fabien.eth',
     flagged: false,
     verified: true,
+    turbo: false,
+    hibernated: false,
     settings: { network: 1 },
     created: Math.floor(Date.now() / 1e3),
     updated: Math.floor(Date.now() / 1e3)
