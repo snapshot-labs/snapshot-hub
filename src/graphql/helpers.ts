f2ffb96bb2a97fa5a92db80b0f641dc6e2c8fb71
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-01-12 19:32:39 +0700

    feat: add `trubo` filter to spaces

--- src/graphql/helpers.ts
+++ src/graphql/helpers.ts
@@ -176,7 +176,7 @@ export function buildWhereQuery(fields, alias, where) {
 export async function fetchSpaces(args) {
   const { first = 20, skip = 0, where = {} } = args;
 
-  const fields = { id: 'string', created: 'number' };
+  const fields = { id: 'string', created: 'number', turbo: 'boolean' };
   const whereQuery = buildWhereQuery(fields, 's', where);
   const queryStr = whereQuery.query;
   const params: any[] = whereQuery.params;
