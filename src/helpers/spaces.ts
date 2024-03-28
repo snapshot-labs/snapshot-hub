cedb558e50a8e5f1f212fed0b79092d037fb6153
Chaitanya <yourchaitu@gmail.com>
2024-03-28 10:57:26 +0530

    [fix] Reduce wait time to two mins while loading spaces (#831)

--- src/helpers/spaces.ts
+++ src/helpers/spaces.ts
@@ -209,6 +209,6 @@ export default async function run() {
     capture(e);
     log.error(`[spaces] failed to load spaces, ${JSON.stringify(e)}`);
   }
-  await snapshot.utils.sleep(360e3);
+  await snapshot.utils.sleep(120e3);
   run();
 }
