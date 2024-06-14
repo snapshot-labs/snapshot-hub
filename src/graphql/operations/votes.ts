f5a70ce7f23672202186bdee39104cb83680d7a4
ChaituVR <yourchaitu@gmail.com>
2024-06-14 11:15:15 +0530

    fix: Allow lowercase voter address on votes query

--- src/graphql/operations/votes.ts
+++ src/graphql/operations/votes.ts
@@ -21,7 +21,7 @@ async function query(parent, args, context?, info?) {
     id: 'string',
     ipfs: 'string',
     space: 'string',
-    voter: 'string',
+    voter: 'EVMAddress',
     proposal: 'string',
     reason: 'string',
     app: 'string',
