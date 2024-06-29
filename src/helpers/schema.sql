1a61702e715e8ddac1bd399f71f1ff3083919dac
ChaituVR <yourchaitu@gmail.com>
2024-06-29 12:17:15 +0530

    Fix leaderboard

--- src/helpers/schema.sql
+++ src/helpers/schema.sql
@@ -166,3 +166,16 @@ CREATE TABLE statements (
   INDEX updated (updated),
   INDEX status (status)
 );
+
+CREATE TABLE leaderboard (
+  user VARCHAR(100) NOT NULL,
+  space VARCHAR(64) NOT NULL,
+  vote_count SMALLINT UNSIGNED NOT NULL DEFAULT '0',
+  proposal_count SMALLINT UNSIGNED NOT NULL DEFAULT '0',
+  last_vote BIGINT,
+  PRIMARY KEY user_space (user,space),
+  INDEX space (space),
+  INDEX vote_count (vote_count),
+  INDEX proposal_count (proposal_count),
+  INDEX last_vote (last_vote)
+);
