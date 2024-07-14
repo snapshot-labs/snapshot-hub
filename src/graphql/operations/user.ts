c2fac2c49c8568ab10f9137789fd655056a6fcb2
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-07-14 11:46:36 +0900

    fix: count leaderboard entry as valid user

--- src/graphql/operations/user.ts
+++ src/graphql/operations/user.ts
@@ -6,19 +6,38 @@ import { capture } from '@snapshot-labs/snapshot-sentry';
 export default async function (parent, args) {
   const id = formatAddress(args.id);
   const query = `
-    SELECT
-      u.*,
-      SUM(l.vote_count) as votesCount,
-      SUM(l.proposal_count) as proposalsCount,
-      MAX(l.last_vote) as lastVote
-    FROM users u
-    LEFT JOIN leaderboard l ON l.user = u.id
-    WHERE id = ?
+    SELECT * FROM (
+      SELECT
+        u.*,
+        u.id as userId,
+        COALESCE(SUM(l.vote_count), 0) as votesCount,
+        COALESCE(SUM(l.proposal_count), 0) as proposalsCount,
+        MAX(l.last_vote) as lastVote
+      FROM users u
+      LEFT JOIN leaderboard l ON l.user = u.id
+      GROUP BY u.id
+      UNION
+      SELECT
+        u.*,
+        l.user as userId,
+        COALESCE(SUM(l.vote_count), 0) as votesCount,
+        COALESCE(SUM(l.proposal_count), 0) as proposalsCount,
+        MAX(l.last_vote) as lastVote
+      FROM users u
+      RIGHT JOIN leaderboard l ON l.user = u.id
+      GROUP BY l.user
+    ) AS t
+    WHERE t.userId = ?
     LIMIT 1`;
   try {
-    const users = await db.queryAsync(query, id);
-    if (users.length === 1) return formatUser(users[0]);
-    return null;
+    const users = await db.queryAsync(query, [id, id]);
+
+    if (!users.length) return null;
+
+    users[0].id = users[0].userId;
+    delete users[0].userId;
+
+    return formatUser(users[0]);
   } catch (e: any) {
     log.error(`[graphql] user, ${JSON.stringify(e)}`);
     capture(e, { args });
