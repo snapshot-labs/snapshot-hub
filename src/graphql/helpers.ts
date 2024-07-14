0b588e53c1df47d497d10acadba3993c68858de0
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-07-14 12:38:30 +0900

    fix: fix invalid id assignment

--- src/graphql/helpers.ts
+++ src/graphql/helpers.ts
@@ -326,12 +326,12 @@ export async function handleRelatedSpaces(info: any, spaces: any[]) {
 export function formatUser(user) {
   const profile = jsonParse(user.profile, {});
   delete user.profile;
+  user.id = user.userId || user.id;
   delete user.userId;
 
   return {
     ...user,
-    ...profile,
-    id: user.userId || user.id
+    ...profile
   };
 }
 
