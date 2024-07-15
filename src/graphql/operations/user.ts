0fdc458f126919877b0153a84a4c59f29dee38ab
Wan <495709+wa0x6e@users.noreply.github.com>
2024-07-15 23:28:48 +0900

    hotfix: return only user if exists (#890)

--- src/graphql/operations/user.ts
+++ src/graphql/operations/user.ts
@@ -6,16 +6,9 @@ import { formatUser, formatAddresses, PublicError } from '../helpers';
 export default async function (parent, args) {
   const addresses = formatAddresses([args.id]);
   if (!addresses.length) throw new PublicError('Invalid address');
-  const query = `
-    SELECT
-      u.*,
-      SUM(l.vote_count) as votesCount,
-      SUM(l.proposal_count) as proposalsCount,
-      MAX(l.last_vote) as lastVote
-    FROM users u
-    LEFT JOIN leaderboard l ON l.user = u.id
-    WHERE id = ?
-    LIMIT 1`;
+
+  const query = `SELECT u.* FROM users u WHERE id = ? LIMIT 1`;
+
   try {
     const users = await db.queryAsync(query, addresses[0]);
     if (users.length === 1) return formatUser(users[0]);
