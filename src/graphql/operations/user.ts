3981a8c1ffe32a3ff70f046c5e323c04a090a1ca
ChaituVR <yourchaitu@gmail.com>
2024-07-17 13:02:53 +0530

    fix user query

--- src/graphql/operations/user.ts
+++ src/graphql/operations/user.ts
@@ -4,10 +4,11 @@ import users from './users';
 export default async function (parent, args) {
   const addresses = formatAddresses([args.id]);
   if (!addresses.length) throw new PublicError('Invalid address');
-
-  return await users(parent, {
+  const usersObject = await users(parent, {
     first: 1,
     skip: 0,
     where: { id: addresses[0] }
   });
+
+  return usersObject[0] || null;
 }
