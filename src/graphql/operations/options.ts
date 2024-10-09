c3e48c9320404b9f3ee3c59d9a55e26d7ceb43e0
Chaitanya <yourchaitu@gmail.com>
2024-10-09 11:54:33 +0530

    Update src/graphql/operations/options.ts

--- src/graphql/operations/options.ts
+++ src/graphql/operations/options.ts
@@ -1,5 +1,5 @@
-import snapshot from '@snapshot-labs/snapshot.js';
 import { capture } from '@snapshot-labs/snapshot-sentry';
+import snapshot from '@snapshot-labs/snapshot.js';
 import log from '../../helpers/log';
 import db from '../../helpers/mysql';
 
