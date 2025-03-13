c6aa01e1b103db7189026f17f9983297a00c968d
Less <bonustrack@users.noreply.github.com>
2025-03-13 13:48:38 +0700

    feat: add network names (#986)
    * feat: add network names
    
    * chore: reset count on new loading
    
    * chore: update networks table schema
    
    * chore: add testnet param
    
    * Change premium column type to TINYINT

--- src/graphql/operations/networks.ts
+++ src/graphql/operations/networks.ts
@@ -1,45 +1,14 @@
 import db from '../../helpers/mysql';
-import { spaces } from '../../helpers/spaces';
+import { networkSpaceCounts } from '../../helpers/spaces';
 
-export default async function () {
-  const networks: any = Object.values(spaces).reduce((acc: any, space: any) => {
-    const spaceNetworks = [
-      space.network,
-      ...space.strategies.map((strategy: any) => strategy.network),
-      ...space.strategies.flatMap((strategy: any) =>
-        Array.isArray(strategy.params?.strategies)
-          ? strategy.params.strategies.map((param: any) => param.network)
-          : []
-      )
-    ];
+export default async function (): Promise<any[]> {
+  let networks = await db.queryAsync('SELECT * FROM networks');
 
-    // remove undefined and duplicates
-    const uniqueNetworks = [...new Set(spaceNetworks)].filter(Boolean);
+  networks = networks.map(network => ({
+    ...network,
+    premium: !!network.premium,
+    spacesCount: networkSpaceCounts[network.id] || 0
+  }));
 
-    uniqueNetworks.forEach(network => {
-      acc[network] = acc[network] ? acc[network] + 1 : 1;
-    });
-
-    return acc;
-  }, {});
-
-  const results = Object.entries(networks).reduce((acc, [id, spacesCount]) => {
-    acc[id] = {
-      id,
-      spacesCount: spacesCount,
-      premium: false
-    };
-
-    return acc;
-  }, {});
-
-  const premiumNetworks = await db.queryAsync(
-    'SELECT * FROM networks WHERE premium = 1'
-  );
-  premiumNetworks.forEach((network: any) => {
-    results[network.id] ||= { id: network.id, spacesCount: 0 };
-    results[network.id].premium = true;
-  });
-
-  return Object.values(results);
+  return networks;
 }
